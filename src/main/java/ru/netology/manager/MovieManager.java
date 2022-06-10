d5e336ce1f7b5eb34695d2fae10f8cb76d6fbf2b
trukhachev495 <sergey.trainings@gmail.com>
2022-06-10 20:47:22 +0300

    version2

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,2 +1,47 @@
-package ru.netology.manager;public class MovieManager {
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] items = new Movie[0];
+    private int limit = 10;
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public MovieManager() {
+    }
+
+    public void save(Movie item) {
+        int length = items.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(items,0,tmp,0,items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public Movie[] findAll() {
+        return items;
+    }
+
+    public Movie[] findLast() {
+        int resultLenght;
+        if (limit > items.length) {
+            resultLenght = items.length;
+        } else {
+            resultLenght = limit;
+        }
+        Movie[] result = new Movie[resultLenght];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+
+        return result;
+    }
+
 }
+
+
--- src/test/java/ru/netology/domain/MovieManagerTest.java
+++ src/test/java/ru/netology/domain/MovieManagerTest.java
@@ -1,2 +1,85 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.manager.MovieManager;
+
 public class MovieManagerTest {
+    Movie first = new Movie("100", "bloodshot", "action movie");
+    Movie second = new Movie("200", "forward", "cartoon");
+    Movie third = new Movie("300", "belgrade", "comedy");
+    Movie fourth = new Movie("400", "gentlemen", "action movie");
+    Movie fifth = new Movie("500", "invisibleMan", "horror");
+    Movie sixth = new Movie("600", "trollsWorldTour", "cartoon");
+    Movie seventh = new Movie("700", "numberOne", "comedy");
+
+    MovieManager manager = new MovieManager();
+
+
+    @Test
+    public void addingTwo() {
+        manager.save(first);
+        manager.save(second);
+
+        Movie[] actual = manager.findAll();
+        Movie[] expected = {first, second};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findAllEmpty() {
+        Movie[] actual = manager.findAll();
+        Movie[] expected = {};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLastDefault() {
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+
+        Movie[] actual = manager.findLast();
+        Movie[] expected = {seventh, sixth, fifth, fourth, third, second, first};
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLast() {
+        MovieManager manager = new MovieManager(2);
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+
+        Movie[] actual = manager.findLast();
+        Movie[] expected = {seventh, sixth};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLastLimitOverLenght() {
+
+        MovieManager manager = new MovieManager(6);
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+
+        Movie[] actual = manager.findLast();
+        Movie[] expected = {third, second, first};
+
+        Assertions.assertArrayEquals(actual, expected);
+
+    }
+
 }
